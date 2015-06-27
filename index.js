3de489932e6369fd13749e4154349e556c4ede01
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-06-27 23:40:52 +0200

    Trying to use url path instead of query

--- index.js
+++ index.js
@@ -6,61 +6,47 @@ var ical = require('ical-generator'),
 
 http.createServer(function(req, res) {
     var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
 
-    var subjects = query["subjects"];
-
-    if (subjects) {
-        try {
-            subjects = JSON.parse(query["subjects"]);
-        } catch (e) {
-            subjects = [];
-        }
-
-        var lectures = [];
-        var names = [];
-
-        for (subject of subjects) {
-            try {
-                var file = fs.readFileSync('lectures/' + subject + '.json');
-                var sub = JSON.parse(file.toString());
-
-                lectures = lectures.concat(sub.Lectures);
-                names.push(sub.Name);
-            } catch (e) {
-                continue;
-            }
-        }
-
-        var cal = ical({
-            domain: 'github.com',
-            name: names.toString()
-        });
-
-        for (lecture of lectures) {
-            cal.createEvent({
-                start: new Date(lecture.Date),
-                end: new Date(new Date(lecture.Date).getTime() + lecture.Length * 3600000),
-                summary: lecture.Name,
-                description: lecture.Lecturers,
-                location: lecture.Rooms
-            });
-        }
-
-
-        res.writeHead(200, {
-            "Content-Type": "text/plain; charset=utf-8"
-        });
-        res.end(cal.toString());
-
-        return;
+    try {
+        subjects = url_parts.pathname.substring(1).split("/")
+    } catch (e) {
+        subjects = [];
     }
 
+    var lectures = [];
+    var names = [];
 
-    res.writeHead(500, {
-        "Content-Type": "text/plain"
+    for (subject of subjects) {
+        try {
+            var file = fs.readFileSync('lectures/' + decodeURIComponent(subject) + '.json');
+            var sub = JSON.parse(file.toString());
+
+            lectures = lectures.concat(sub.Lectures);
+            names.push(sub.Name);
+        } catch (e) {
+            continue;
+        }
+    }
+
+    var cal = ical({
+        domain: 'github.com',
+        name: names.toString()
     });
-    res.end("error");
+
+    for (lecture of lectures) {
+        cal.createEvent({
+            start: new Date(lecture.Date),
+            end: new Date(new Date(lecture.Date).getTime() + lecture.Length * 3600000),
+            summary: lecture.Name,
+            description: lecture.Lecturers,
+            location: lecture.Rooms
+        });
+    }
+
+    res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8"
+    });
+    res.end(cal.toString());
 }).listen(port, function() {
     console.log('Server running at http://127.0.0.1:' + port + '/');
 });
