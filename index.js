411a95809a378ec39872510e159b21de5ee1ffa0
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-06-27 20:57:30 +0200

    Trying to serve calendar as plain text

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ cal.createEvent({
     url: 'https://github.com/s111/icalgen'
 });
 
-console.log(process.env.PORT || 5000);
+console.log(cal.toString());
 
 var port = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
-    cal.serve(res);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(cal.toString());
 }).listen(port, function() {
     console.log('Server running at http://127.0.0.1:' + port + '/');
 });
